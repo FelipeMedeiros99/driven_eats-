6c4faf95a6b5703b9cefc00c6d22d3c913eef8aa
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2023-09-14 13:30:07 -0300

    resolvido o bug da mudança de pedido, agora é possível mudar e a mensagem do whatsapp acompanhar

--- configuracoes.js
+++ configuracoes.js
@@ -5,18 +5,71 @@ function selecionado(item, secao){
     */
 
 
-    // removendo pratos caso já possua um selecionado
-    const caixa = document.querySelector(secao)
-    const itemJaSelecionados =  caixa.querySelector(".selecionados")
+    //1 - removendo pratos selecionados na seção, caso existam
+    const caixa = document.querySelector(secao) //selecionando a seção (bebidas, sobremesas ou comidas)
+    const itemJaSelecionados =  caixa.querySelector(".selecionados") //localizando item selecionado na seção
+    
+    // se existir um ítem selecionado, ele é removido
     if (itemJaSelecionados !== null){
         itemJaSelecionados.classList.remove("selecionados")
     }
 
     // selecionando o novo item
-    const pratoSelecionado = document.querySelector(item)
-    pratoSelecionado.classList.add("selecionados")
-    caixa.classList.add("ok")
-    const linkZap = ativaBotao()
+    const novoSelecionado = caixa.querySelector(item) 
+    novoSelecionado.classList.add("selecionados") //adicionando a classe selecionados 
+    caixa.classList.add("ok") //adiciona o "ok" na seção principal
+    
+    // testando se todas as seções já foram selecionadas
+    ativaBotao()
+
+}
+
+function ativaBotao(){
+    //funcao verifica se todos as seçoes estão selecionadas. Caso estejam
+    //muda o botão para fechar o pedido 
+
+
+    const setorPratos = document.querySelector(".secao-pratos")
+    const setorBebidas = document.querySelector(".secao-bebidas")
+    const setorSobremesa = document.querySelector(".secao-sobremesas")
+    
+    const pratoEstaSelecionado = setorPratos.classList.contains('ok')
+    const bebidaEstaSelecionada = setorBebidas.classList.contains('ok')
+    const sobremesaEstaSelecionada = setorSobremesa.classList.contains('ok')
+
+    // verificando se todos estão selecionados pela classe ".ok"
+    if (pratoEstaSelecionado && bebidaEstaSelecionada && sobremesaEstaSelecionada){
+        const botao = document.querySelector("button")
+        document.querySelector("a").classList.add("link-on")
+        botao.classList.add("ativado")
+        botao.innerHTML = "fechar pedido"
+
+        
+        botao2 = document.querySelector(".botaoSecundario");
+        if (botao2 !== null){
+            botao2.classList.add('desativar');
+        }
+
+        const prato = gerenciaPedidos(".secao-pratos")
+        const precoPrato = parseFloat(preçoFinal(".secao-pratos").replace(",", ".").replace("$", ""))
+        const bebida = gerenciaPedidos(".secao-bebidas")
+        const precoBebida = parseFloat(preçoFinal(".secao-bebidas").replace(",", ".").replace("$", ""))
+        const sobremesa = gerenciaPedidos(".secao-sobremesas")
+        const precoSobremesa = parseFloat(preçoFinal(".secao-sobremesas").replace(",", ".").replace("$", ""))
+        let soma = (precoBebida+precoPrato+precoSobremesa)
+        
+        let mensagem = `Olá, gostaria de fazer o pedido:
+        - Prato: ${prato}
+        - Bebida: ${bebida}
+        - Sobremesa: ${sobremesa}
+        Total: R$ ${((precoBebida+precoPrato+precoSobremesa).toFixed(2)+"").replace(".", ",")}`
+        
+        mensagem = encodeURIComponent(mensagem)
+        //alert(mensagem)
+        const ancoragem = `<a target="_blank" class="link-on" href="https://api.whatsapp.com/send?phone=5598987835523&text=${mensagem}"><button class="ativado">fechar pedido</button></a>`
+        document.querySelector("footer").innerHTML = ancoragem   
+    }
+
 
 }
 
@@ -33,47 +86,6 @@ function gerenciaPedidos(secao){
     }
     
 }
-
-
-function ativaBotao(){
-    const setorPratos = document.querySelector(".secao-pratos")
-    const setorBebidas = document.querySelector(".secao-bebidas")
-    const setorSobremesa = document.querySelector(".secao-sobremesas")
-    
-    const pratoSelecionado = setorPratos.classList.contains('ok')
-    const bebidaSelecionada = setorBebidas.classList.contains('ok')
-    const sobremesaSelecionada = setorSobremesa.classList.contains('ok')
-
-    if (pratoSelecionado && bebidaSelecionada && sobremesaSelecionada){
-        const botao = document.querySelector("button")
-        document.querySelector("a").classList.add("link-on")
-        botao.classList.add("ativado")
-        botao.innerHTML = "fechar pedido"
-
-        botao2 = document.querySelector(".botaoSecundario");
-        botao2.classList.add('desativar');
-
-        const prato = gerenciaPedidos(".secao-pratos")
-        const precoPrato = parseFloat(preçoFinal(".secao-pratos").replace(",", ".").replace("$", ""))
-        const bebida = gerenciaPedidos(".secao-bebidas")
-        const precoBebida = parseFloat(preçoFinal(".secao-bebidas").replace(",", ".").replace("$", ""))
-        const sobremesa = gerenciaPedidos(".secao-sobremesas")
-        const precoSobremesa = parseFloat(preçoFinal(".secao-sobremesas").replace(",", ".").replace("$", ""))
-        let mensagem = `Olá, gostaria de fazer o pedido:
-        - Prato: ${prato}
-        - Bebida: ${bebida}
-        - Sobremesa: ${sobremesa}
-        Total: R$ ${precoBebida+precoPrato+precoSobremesa}`
-        
-        mensagem = encodeURIComponent(mensagem)
-        //alert(mensagem)
-        const ancoragem = `<a class="link-on" href="https://api.whatsapp.com/send?phone=5598987835523&text=${mensagem}"><button class="ativado">fechar pedido</button></a>`
-        document.querySelector("footer").innerHTML = ancoragem   
-    }
-
-
-}
-
 
 function preçoFinal(secao){
 
